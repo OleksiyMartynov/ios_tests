6f7b6df14edcc524e28d1b18d53b13dff16d6f75
Oleksiy Martynov <3martynov@gmail.com>
2015-01-07 20:43:34 -0500

    Initial Commit

---
+++ EmptyApp/MyAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  MyAppDelegate.h
+//  EmptyApp
+//
+//  Created by Oleksiy Martynov on 1/7/15.
+//  Copyright (c) 2015 Oleksiy Martynov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MyAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ EmptyApp/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  EmptyApp
+//
+//  Created by Oleksiy Martynov on 1/7/15.
+//  Copyright (c) 2015 Oleksiy Martynov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "MyAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([MyAppDelegate class]));
+    }
+}
